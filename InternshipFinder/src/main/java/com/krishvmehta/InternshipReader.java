ec81216e3a21b3afb637e938fdc8823377d51654
Krish Mehta <me@krishvmehta.com>
2023-12-26 12:05:56 +0800

    Refactor JSON parsing in InternshipReader
    Moved JSON object conversion to a new private method in InternshipReader class. This makes the parseJSONFile method cleaner and more readable, improving maintainability and easier debugging in the long run.

--- InternshipFinder/src/main/java/com/krishvmehta/InternshipReader.java
+++ InternshipFinder/src/main/java/com/krishvmehta/InternshipReader.java
@@ -18,28 +18,30 @@ public class InternshipReader {
 
     public List<Internship> parseJSONFile() throws IOException {
         List<Internship> internships = new ArrayList<>();
-
         Path path = Paths.get(".github/scripts/listings.json");
         String json = Files.readString(path);
         JSONArray jsonArray = new JSONArray(json);
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
-            String company = jsonObject.getString("company_name");
-            String title = jsonObject.getString("title");
-            List<String> locations = new ArrayList<>();
-            JSONArray locationsArray = jsonObject.getJSONArray("locations");
-            for (int j = 0; j < locationsArray.length(); j++) {
-                locations.add(locationsArray.getString(j));
-            }
-            String url = jsonObject.getString("url");
-            String companyURL = jsonObject.getString("company_url");
-            boolean active = jsonObject.getBoolean("active");
-            boolean visible = jsonObject.getBoolean("is_visible");
-            Internship internship = new Internship(company, title, locations, url, companyURL, active, visible);
+            Internship internship = getInternshipFromJSONObject(jsonObject);
             internships.add(internship);
         }
-
         return internships;
+    }
+
+    private Internship getInternshipFromJSONObject(JSONObject jsonObject) {
+        String company = jsonObject.getString("company_name");
+        String title = jsonObject.getString("title");
+        List<String> locations = new ArrayList<>();
+        JSONArray locationsArray = jsonObject.getJSONArray("locations");
+        for (int j = 0; j < locationsArray.length(); j++) {
+            locations.add(locationsArray.getString(j));
+        }
+        String url = jsonObject.getString("url");
+        String companyURL = jsonObject.getString("company_url");
+        boolean active = jsonObject.getBoolean("active");
+        boolean visible = jsonObject.getBoolean("is_visible");
+        return new Internship(company, title, locations, url, companyURL, active, visible);
     }
 
 }
