5867460b65b02f3d0dbb2ebcc71bc6a9f121f9f0
Krish Mehta <me@krishvmehta.com>
2023-12-28 17:04:34 +0800

    Change directory in sync_fork.sh script
    The script was updated to change the current working directory to the directory of the script being executed.

--- .github/scripts/sync_fork.sh
+++ .github/scripts/sync_fork.sh
@@ -3,6 +3,8 @@
 UPSTREAM_REPO="https://github.com/SimplifyJobs/Summer2024-Internships"
 BRANCH="dev"
 
+cd "$(dirname "$0")" || exit
+
 if ! git remote | grep -q upstream; then
     git remote add upstream $UPSTREAM_REPO
 fi
